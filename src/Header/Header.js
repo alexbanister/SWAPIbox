8a48c32d945a773fc0b0333dcec8136c251ce0cd
johnmboudreaux <jhnbdrx@gmail.com>
2017-10-12 18:08:22 -0600

    lint cleaned header.js and fixed proptypes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.svg';
 
-const Header = ({favCount}) => {
+const Header = ({ favCount }) => {
   return (
     <div className='header'>
       <a href="/"><img src={logo} alt='logo' /></a>
       <nav>
-        <NavLink to='/people' activeClassName="active">People</NavLink>
-        <NavLink to='/planets' activeClassName="active">Planets</NavLink>
-        <NavLink to='/vehicles' activeClassName="active">Vehicles</NavLink>
-        <NavLink to='/favorites' activeClassName="active">Favorites <i>({favCount})</i></NavLink>
+        <NavLink
+          to='/people'
+          activeClassName="active">People</NavLink>
+        <NavLink
+          to='/planets'
+          activeClassName="active">Planets</NavLink>
+        <NavLink
+          to='/vehicles'
+          activeClassName="active">Vehicles</NavLink>
+        <NavLink
+          to='/favorites'
+          activeClassName="active">Favorites <i>({favCount})</i></NavLink>
       </nav>
     </div>
   );
@@ -19,7 +27,8 @@ const Header = ({favCount}) => {
 
 Header.propTypes = {
   compareInfo: PropTypes.objectOf(PropTypes.object),
-  removeCompare: PropTypes.func
+  removeCompare: PropTypes.func,
+  favCount: PropTypes.number
 };
 
 export default Header;
