8e8e9c9ad8d6dc2904a92d9f4237e749b54f9275
Alex Banister <alexbanister@gmail.com>
2017-10-13 14:50:38 -0600

    still not working

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,133 +1,49 @@
 import React from 'react';
+import createRouterContext from 'react-router-test-context';
 import { shallow, mount } from 'enzyme';
 import fetchMock from 'fetch-mock';
-import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 import {
   mockFilms,
   mockPeople,
   mockPlanets,
   mockVehicles,
-  mockSpecies
+  mockSpecies,
+  mockPerson,
+  mockPlanet
 } from '../_mock/mockData';
-
 
 describe('User intergration test', () => {
 
   beforeEach(() => {
-    fetchMock.get('https://swapi.co/api/films/1', {
+    fetchMock.get('https://swapi.co/api/films/', {
       status: 200,
       body: mockFilms
-    });
-    fetchMock.get('https://swapi.co/api/films/2', {
-      status: 200,
-      body: mockFilms
-    });
-    fetchMock.get('https://swapi.co/api/films/3', {
-      status: 200,
-      body: mockFilms
-    });
-    fetchMock.get('https://swapi.co/api/films/4', {
-      status: 200,
-      body: mockFilms
-    });
-    fetchMock.get('https://swapi.co/api/films/5', {
-      status: 200,
-      body: mockFilms
-    });
-    fetchMock.get('https://swapi.co/api/films/6', {
-      status: 200,
-      body: mockFilms
-    });
-    fetchMock.get('https://swapi.co/api/films/7', {
-      status: 200,
-      body: mockFilms
-    });
-    fetchMock.get('https://swapi.co/api/planets/', {
-      status: 200,
-      body: mockPlanets
-    });
-    fetchMock.get('https://swapi.co/api/people/5/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/68/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/81/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/26/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/30/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/3/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/34/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/55/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/74/', {
-       status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/74/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/72/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/73/', {
-      status: 200,
-      body: mockPeople
-    });
-    fetchMock.get('https://swapi.co/api/people/63/', {
-      status: 200,
-      body: mockPeople
-    });
-    // vehicles
-    fetchMock.get('https://swapi.co/api/vehicles/', {
-      status: 200,
-      body: mockVehicles
     });
     fetchMock.get('https://swapi.co/api/people/', {
       status: 200,
       body: mockPeople
     });
-    //planets
-    fetchMock.get('https://swapi.co/api/planets/1/', {
+    fetchMock.get('https://swapi.co/api/planets/', {
       status: 200,
       body: mockPlanets
     });
-    fetchMock.get('https://swapi.co/api/species/1/', {
+    fetchMock.get('https://swapi.co/api/vehicles/', {
+      status: 200,
+      body: mockVehicles
+    });
+
+    fetchMock.get('https://enzymeSucks.com/mockData/api/planets/1/', {
+      status: 200,
+      body: mockPlanet
+    });
+    fetchMock.get('https://enzymeSucks.com/mockData/api/species/1/', {
       status: 200,
       body: mockSpecies
     });
-    fetchMock.get('https://swapi.co/api/species/2/', {
+    fetchMock.get('https://enzymeSucks.com/mockData/api/people/5/', {
       status: 200,
-      body: mockSpecies
-    });
-    fetchMock.get('https://swapi.co/api/planets/8/', {
-      status: 200,
-      body: mockPlanets
-    });
-    fetchMock.get('https://swapi.co/api/planets/20/', {
-      status: 200,
-      body: mockPlanets
+      body: mockPerson
     });
   });
   afterEach(() => {
@@ -135,11 +51,27 @@ describe('User intergration test', () => {
     fetchMock.restore();
   });
   it('should render Header and Welcome components', () => {
-    const wrapper = shallow(<Router><App /></Router>);
-
+    const wrapper = shallow(<App />);
     expect(wrapper.find('Header').length).toEqual(1);
     expect(wrapper.find('Welcome').length).toEqual(1);
   });
+  it.skip('should render CardContainer components on a route', () => {
+    const context = createRouterContext();
+    // const wrapper = shallow(<MyComponent />, { context })
+    const wrapper = mount(<App />, { context });
+
+    const peopleLink = wrapper.find('/people');
+    peopleLink.simulate('click');
+
+    let peopleCards = wrapper.find('Card').length;
+
+    expect(peopleCards).toEqual(10);
+
+    const loadMore = wrapper.find('.button');
+    loadMore.simulate('click');
+
+    peopleCards = wrapper.find('Card').length;
+    expect(peopleCards).toEqual(20);
+  });
 
 });
-
