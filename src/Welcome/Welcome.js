6492c2d766bdbb80446c37ed6f901424ba2ed16d
johnmboudreaux <jhnbdrx@gmail.com>
2017-10-12 18:11:32 -0600

    lint welcome cleaned and fixed proptypes

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -3,24 +3,19 @@ import PropTypes from 'prop-types';
 
 const Welcome = ({ movie }) => {
 
-  const romanArray = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
-  
+  const romanArray = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+
   return (
     <div className='welcome-body'>
       <div className="fade"></div>
       <section className="star-wars">
-
         <div className="crawl">
-
           <div className="title">
             <p>{ romanArray[movie.EpisodeId] }</p>
             <h1>{ movie.title }</h1>
           </div>
-
           <p>{ movie.openingCrawl }</p>
-
         </div>
-
       </section>
     </div>
   );
@@ -28,7 +23,8 @@ const Welcome = ({ movie }) => {
 
 Welcome.propTypes = {
   compareInfo: PropTypes.objectOf(PropTypes.object),
-  removeCompare: PropTypes.func
+  removeCompare: PropTypes.func,
+  movie: PropTypes.object
 };
 
 export default Welcome;
